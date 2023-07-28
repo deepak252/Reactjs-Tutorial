f54d3d0e2ef1994e33712a91d8ae61a58c1da1c3
deepak252 <trojanhorsedc@gmail.com>
2023-07-28 14:22:13 +0530

    HOC min. code

--- src/components/withCounter.js
+++ src/components/withCounter.js
@@ -1,28 +1,36 @@
 import { useState, Component} from "react";
 
 // Higher Order Component
-const withCounter=(WrappedComponent)=> {
-    
-    // class NewComponent extends Component{
-    //     const [count, setCount] = useState(0);
-    //     const incrementCount = () => {
-    //         setCount((val) => val + 1);
-    //     };
-    //     render(){
-    //         return <OriginalComponent name = "Deepak"/>
-    //     }
-    // }
-
-    const WithCounter = (props)=>{
+const withCounter=(WrappedComponent)=> (props)=>{
         const [count, setCount] = useState(0);
         const incrementCount = () => {
             setCount((val) => val + 1);
         };
-
         return <WrappedComponent count={count} incrementCount={incrementCount} />
-    }
-
-    return WithCounter
 }
+//// 2.
+// const withCounter=(WrappedComponent)=> {
+//     const WithCounter = (props)=>{
+//         const [count, setCount] = useState(0);
+//         const incrementCount = () => {
+//             setCount((val) => val + 1);
+//         };
+//         return <WrappedComponent count={count} incrementCount={incrementCount} />
+//     }
+//     return WithCounter
+// }
+//// 3. 
+// const withCounter=(WrappedComponent)=> {
+//     class NewComponent extends Component{
+//         const [count, setCount] = useState(0);
+//         const incrementCount = () => {
+//             setCount((val) => val + 1);
+//         };
+//         render(){
+//             return <OriginalComponent name = "Deepak"/>
+//         }
+//     }
+//     return WithCounter
+// }
 
 export default withCounter;
