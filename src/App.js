1a361a2ce108f28dacd9c29b02dd626964b6a511
deepak252 <trojanhorsedc@gmail.com>
2023-07-28 12:14:42 +0530

    Error Boundary Example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import ErrorBoundary from "./components/ErrorBoundary";
+import Hero from "./components/Hero";
 
 function App() {
     return (
         <div className="App">
-            
+            <ErrorBoundary>
+                <Hero heroName="Batman"/>
+            </ErrorBoundary>
+            <ErrorBoundary>
+                <Hero heroName="Superman"/>
+            </ErrorBoundary>
+            <ErrorBoundary>
+                <Hero heroName="Joker"/>
+            </ErrorBoundary>
         </div>
     );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+
+function Hero({ heroName }) {
+    if(heroName==='Joker'){
+        throw new Error("Something went wrong")
+    }
+    return <div>{heroName}</div>;
+}
+
+export default Hero;
