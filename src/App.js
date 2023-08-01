bc7c0bdcb5ca6626b40994280f557451ae27ce9a
deepak252 <trojanhorsedc@gmail.com>
2023-08-01 12:55:51 +0530

    UseState rendering test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import UseState from "./components/UseState/UseState.js";
 
 function App() {
+    console.log("App Render");
     return (
         <div className="App">
-            
+            <UseState />
         </div>
     );
 }
---
+++ src/components/UseState/UseState.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function UseState() {
+    const [count,setCount] = useState(0);
+
+    console.log("UseState Render");
+
+    return (
+        <div>
+            <h1> Count = {count}</h1>
+            <button onClick={()=>setCount(c=>c+1)}>Increment</button>
+            <button onClick={()=>setCount(0)}>Set to 0</button>
+            <button onClick={()=>setCount(5)}>Set to 5</button>
+        </div>
+    )
+}
+
+export default UseState;
